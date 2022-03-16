7fd3f74d160d362a638ff45fe3ad4b6bbd2d729c
Celestin25 <celestinhakorimana25@gmail.com>
2022-03-16 23:04:11 +0200

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
-#include "holberton.h"
+#include "main.h"
+
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	char h[] = "Holberton";
-	int m;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	m = 0;
-
-	while (m <= 8)
-	{
-		_putchar(h[m]);
-		m++;
-	}
-	_putchar('\n');
-	return (0);
+return (0);
 }
